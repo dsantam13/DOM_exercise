0b67a5b223c3d8b595a3f344ef6b32381dc0f786
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-05-04 18:21:03 -0500

    Hello DOM

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log(document);
+document.querySelector('h1').textContent = 'Hablemos de JavaScript';
+const title = document.querySelector('h1');
+title.innerHTML = 'New and <span class="accent">improved</span> title';
+
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+const list = document.querySelector(".list");
+const markup = technologies
+  .map((technology) => `<li class="list-item">${technology}</li>`)
+    .join("");
+  
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
+
+// Adding all the markup in one operation
+list.innerHTML = markup;
